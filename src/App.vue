217ba8490228d27643155faefdc005833fd3516d
Your Name <you@example.com>
2017-11-04 17:03:39 -0500

    Fade transition

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,37 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
+                <hr>
+                <button class="btn btn-primary" @click="show = !show"> Show Alert</button>
+                <br><br>
+                <transition name="fade">
+                    <div class="alert alert-info" v-if="show">This is an info</div>
+                </transition>
             </div>
         </div>
     </div>
 </template>
-<script></script>
-<style></style>
+<script>
+    export default {
+        data(){
+            return{
+                show: true
+            }
+        }
+    }
+</script>
+<style>
+    .fade-enter{
+        opacity: 0;
+    }
+    .fade-enter-active{
+        transition: opacity 1s;
+    }
+    .fade-leave{
+        /*opacity: 0; */
+    }
+    .fade-leave-active{
+        transition: opacity 1s;
+        opacity: 0;
+    }
+</style>
