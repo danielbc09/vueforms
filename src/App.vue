a8b7d3c67748b709ffdb425cf79bc4f04f63566d
Your Name <you@example.com>
2017-11-04 11:48:31 -0500

    Custms Controller Components

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,8 @@
                     type="text"
                     id="email"
                     class="form-control"
-                    v-model="userData.email">
+                    :value="userData.email"
+                    @input="userData.email = $event.target.value">
           </div>
           <div class="form-group">
             <label for="password">Password</label>
@@ -94,6 +95,11 @@
           </select>
         </div>
       </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-offset-3">
+          <app-switch v-model="dataSwitch"></app-switch>
+        </div>
+      </div>
       <hr>
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
@@ -123,7 +129,7 @@
             </ul>
             <p>Gender: {{ gender }}</p>
             <p>Priority:{{ selectedPriority}}</p>
-            <p>Switched:</p>
+            <p>Switched: {{ dataSwitch }}</p>
           </div>
         </div>
       </div>
@@ -132,6 +138,8 @@
 </template>
 
 <script>
+   import Switch from './Switch.vue';
+
     export default {
         data () {
             return {
@@ -145,7 +153,11 @@
                 gender: 'Male',
                 selectedPriority: 'High',
                 priorities:['High', 'Medium', 'Low'],
+                dataSwitch: true,
             }
+        },
+        components:{
+            'app-switch': Switch,
         }
     }
 </script>
