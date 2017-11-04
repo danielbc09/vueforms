5c0f4bf57e0760c5f76744ebb329b53f2401cf83
Your Name <you@example.com>
2017-11-04 13:00:43 -0500

    Adding modifiers

--- src/main.js
+++ src/main.js
@@ -4,11 +4,17 @@ Vue.directive('highlight',{
   bind(el, binding, vnode){
      // el.style.backgroundColor = 'green';
      //   el.style.backgroundColor = binding.value;
-    if(binding.arg == 'background'){
-        el.style.backgroundColor = binding.value;
-    }else{
-        el.style.color = binding.value;
-    }
+      var delay = 0;
+      if(binding.modifiers['delayed']){
+          delay = 500;
+      }
+      setTimeout(()=>{
+          if(binding.arg === 'background'){
+              el.style.backgroundColor = binding.value;
+          }else{
+              el.style.color = binding.value;
+          }
+      }, delay);
   }
 });
 new Vue({
