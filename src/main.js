97638a8ac7a8743812621ef66213a54b3c41e939
Your Name <you@example.com>
2017-11-04 12:47:47 -0500

    Passing values

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 Vue.directive('highlight',{
   bind(el, binding, vnode){
-      el.style.backgroundColor = 'green';
+     // el.style.backgroundColor = 'green';
+        el.style.backgroundColor = binding.value;
   }
 });
 new Vue({
