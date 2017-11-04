24bd6a677f7ad13ae3dabd04b80618e310386149
Your Name <you@example.com>
2017-11-04 13:31:19 -0500

    Local directives

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,29 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom directives</h1>
                 <p v-highlight:background.delayed="'green'">Color this</p>
+                <p v-local-highlight:background.delayed="'green'">color THat</p>
             </div>
         </div>
     </div>
 </template>
+<script>
+    export default {
+        directives:{
+            'local-highlight':{
+                bind(el, binding, vnode){
+                    var delay = 0;
+                    if(binding.modifiers['delayed']){
+                        delay = 500;
+                    }
+                    setTimeout(()=>{
+                        if(binding.arg === 'background'){
+                            el.style.backgroundColor = binding.value;
+                        }else{
+                            el.style.color = binding.value;
+                        }
+                    }, delay);
+                }
+            }
+        }
+    }
+</script>
