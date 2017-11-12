0c72a17dc697db212ad715718288ad4f5aae7d61
Your Name <you@example.com>
2017-11-12 01:09:05 -0500

    Interceptors for request and response

--- src/main.js
+++ src/main.js
@@ -4,6 +4,19 @@ import App from './App.vue'
 
 Vue.use(VueResource);
 Vue.http.options.root = 'https://vue-http-1e295.firebaseio.com/data.json';
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if(request.method == 'POST'){
+    request.method = 'PUT';
+  }
+  next((response => {
+    response.json = () =>{
+      return {
+        messages: response.body
+      }
+    }
+  }));
+});
 
 new Vue({
   el: '#app',
