742fcc0f3f7e769a596f929757cdce40ea73314b
Your Name <you@example.com>
2017-11-13 14:46:31 -0500

    Setting up the project

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>The Home Page</h1>
+        <hr>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias
+            aliquam at aut cumque delectus est excepturi explicabo hic ipsa
+            laudantium minus nihil nisi officia praesentium, quisquam soluta sun.
+        </p>
+    </div>
+</template>
