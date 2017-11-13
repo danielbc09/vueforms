fc70a1bedcb1908f43d881f29ca41baf71c1b20d
Your Name <you@example.com>
2017-11-13 16:44:29 -0500

    Named Routes

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -11,7 +11,7 @@
     export default{
         methods:{
             navigateToHome(){
-                this.$router.push({path: '/'});
+                this.$router.push({ name: 'home'});
             }
         }
     }
