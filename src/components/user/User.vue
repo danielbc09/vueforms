689125777214469719c2410659eb031daf928ebc
Your Name <you@example.com>
2017-11-13 15:42:08 -0500

    Fetching and getting the url parameter

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,3 +1,22 @@
 <template>
-    <h1>the User Page.</h1>
+    <div>
+        <h1>The User Page.</h1>
+        <hr>
+        <p>Loaded ID : {{ id }}</p>
+        <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
+    </div>
 </template>
+<script>
+    export default{
+        data(){
+          return{
+              id: this.$route.params.id
+          }
+        },
+        methods:{
+            navigateToHome(){
+                this.$router.push({path: '/'});
+            }
+        }
+    }
+</script>
--- src/routes.js
+++ src/routes.js
@@ -2,5 +2,5 @@ import User from './components/user/User.vue';
 import Home from './components/Home.vue';
 export const routes =[
     { path: '', component: Home },
-    { path: '/user', component: User }
+    { path: '/user/:id', component: User }
 ];
