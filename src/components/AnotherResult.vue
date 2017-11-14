6067ca2e2a8f17e08dbc45cac99e02e05026f8da
Your Name <you@example.com>
2017-11-13 22:11:45 -0500

    Mapping Getters properties

--- src/components/AnotherResult.vue
+++ src/components/AnotherResult.vue
@@ -1,13 +1,19 @@
 <template>
-    <p>Counter is: {{ counter }}</p>
+    <div>
+        <p>Counter is: {{ doubleCounter }}</p>
+        <p>Counter of CLicks : {{ stringCounter }}</p>
+    </div>
 </template>
 
 <script>
+    import { mapGetters } from 'vuex';
+
     export default {
-        computed:{
-            counter(){
-                return this.$store.getters.doubleCounter;
-            }
+        computed: {
+            ... mapGetters([
+                'doubleCounter',
+                'stringCounter'
+            ])
         }
     }
 </script>
