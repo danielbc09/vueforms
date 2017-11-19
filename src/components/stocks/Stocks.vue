91ac91c5662b432485c1ebf5dd6eff1644152e50
Your Name <you@example.com>
2017-11-18 23:55:12 -0500

    Setting up the vuex state managment

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -6,18 +6,13 @@
 <script>
     import Stock from './Stock.vue';
     export default {
-        data(){
-            return{
-                stocks:[
-                    {id:1, name:'BMW', price:100},
-                    {id:1, name:'Google', price:200},
-                    {id:1, name:'Apple', price:300},
-                    {id:1, name:'Twitter', price:10},
-                ]
-            }
-        },
         components:{
             'app-stock': Stock,
+        },
+        computed:{
+            stocks(){
+                return this.$store.getters.stocks;
+            }
         }
     }
 </script>
---
+++ src/data/stocks.js
@@ -0,0 +1,6 @@
+export default[
+    {id:1, name:'BMW', price:100},
+    {id:1, name:'Google', price:200},
+    {id:1, name:'Apple', price:300},
+    {id:1, name:'Twitter', price:10},
+]
