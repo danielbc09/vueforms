b0809e2b8bb45e5691d609b0527f66d6edd5db61
Your Name <you@example.com>
2017-11-19 00:34:42 -0500

    Set up the portfolio Stock

--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -5,7 +5,7 @@ const state ={
 
 const mutations = {
     'BUY_STOCK'(state, {stockId, quantity, stockPrice}){
-        const record = state.stocks.find(eleement => element.id == stockId);
+        const record = state.stocks.find(element => element.id == stockId);
         if(record){
             record.quantity += quantity;
         }else{
