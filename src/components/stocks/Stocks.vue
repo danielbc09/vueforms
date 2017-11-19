ec9dbd86a9e23930b0d0c6cdca267b1ea2641225
Your Name <you@example.com>
2017-11-18 22:57:08 -0500

    Creating the Stock Components

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,23 @@
 <template>
-    <h1>The Stocks Component</h1>
+    <div>
+        <app-stock v-for="stock in stocks"></app-stock>
+    </div>
 </template>
+<script>
+    import Stock from './Stock.vue';
+    export default {
+        data(){
+            return{
+                stocks:[
+                    {id:1, name:'BMW', price:100},
+                    {id:1, name:'Google', price:200},
+                    {id:1, name:'Apple', price:300},
+                    {id:1, name:'Twitter', price:10},
+                ]
+            }
+        },
+        components:{
+            'app-stock': Stock,
+        }
+    }
+</script>
