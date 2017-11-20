e07d85e8420b9e3539587f50844c9c0ab9be4bf7
Your Name <you@example.com>
2017-11-19 21:03:53 -0500

    Install Axios and send a POST request

--- src/components/auth/signup.vue
+++ src/components/auth/signup.vue
@@ -69,6 +69,7 @@
 </template>
 
 <script>
+  import axios from 'axios';
   export default {
     data () {
       return {
@@ -102,7 +103,9 @@
           hobbies: this.hobbyInputs.map(hobby => hobby.value),
           terms: this.terms
         }
-        console.log(formData)
+        axios.post('https://vue-axios-645be.firebaseio.com/users.json', formData)
+             .then(response => console.log(response))
+             .catch(error => console.log(error));
       }
     }
   }
